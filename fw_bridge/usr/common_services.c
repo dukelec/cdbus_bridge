6064e196db2194299a934d4c68539cfabd1c1cb5
Duke <d@d-l.io>
2025-05-19 21:25:55 +0800

    update to cdnet protocol v2.0

--- fw_bridge/usr/common_services.c
+++ fw_bridge/usr/common_services.c
@@ -13,10 +13,12 @@ static char cpu_id[25];
 static char info_str[100];
 
 
-void send_frame(cd_frame_t *frame)
+static void send_frame(cd_frame_t *frame, uint8_t p_len)
 {
     frame->dat[1] = frame->dat[0];
     frame->dat[0] = 0xff;
+    frame->dat[2] = p_len + 2;
+    swap(frame->dat[3], frame->dat[4]); // swap src and dst port
     cd_list_put(&d_dev.tx_head, frame);
 }
 
@@ -43,52 +45,46 @@ static void init_info_str(void)
 
 
 // device info
-static void p1_hander(cd_frame_t *frame)
+static void p1_handler(cd_frame_t *frame)
 {
-    uint8_t subs = frame->dat[4];
-    uint8_t frame_len = frame->dat[2];
+    uint8_t *p_dat = frame->dat + 5;
+    uint8_t p_len = frame->dat[2] - 2;
 
-    if (subs == 0 && frame_len == 2) {
-        frame->dat[3] = 0x40;
-        strcpy((char *)frame->dat + 4, info_str);
-        frame->dat[2] = strlen(info_str) + 1;
-        send_frame(frame);
-
+    if (p_len == 0) {
+        strcpy((char *)p_dat, info_str);
+        send_frame(frame, strlen(info_str));
     } else {
         cd_list_put(&frame_free_head, frame);
     }
 }
 
 // flash memory manipulation
-static void p8_hander(cd_frame_t *frame)
+static void p8_handler(cd_frame_t *frame)
 {
-    uint8_t subs = frame->dat[4];
-    uint8_t frame_len = frame->dat[2];
+    uint8_t *p_dat = frame->dat + 5;
+    uint8_t p_len = frame->dat[2] - 2;
 
-    if (subs == 0x2f && frame_len == 10) {
-        uint32_t addr = get_unaligned32(frame->dat + 5);
-        uint32_t len = get_unaligned32(frame->dat + 9);
+    if (*p_dat == 0x2f && p_len == 9) {
+        uint32_t addr = get_unaligned32(p_dat + 1);
+        uint32_t len = get_unaligned32(p_dat + 5);
         uint8_t ret = flash_erase(addr, len);
-        frame->dat[3] = ret ? 0x41 : 0x40;
-        frame->dat[2] = 1;
-        send_frame(frame);
+        *p_dat = ret ? 1 : 0;
+        send_frame(frame, 1);
 
-    } else if (subs == 0x00 && frame_len == 7) {
-        uint32_t addr = get_unaligned32(frame->dat + 5);
+    } else if (*p_dat == 0x00 && p_len == 6) {
+        uint32_t addr = get_unaligned32(p_dat + 1);
         uint8_t *dst_addr = (uint8_t *) addr;
-        uint8_t len = min(frame->dat[9], CDN_MAX_DAT - 1);
-        memcpy(frame->dat + 4, dst_addr, len);
-        frame->dat[3] = 0x40;
-        frame->dat[2] = len + 1;
-        send_frame(frame);
+        uint8_t len = min(p_dat[5], CDN_MAX_DAT - 1);
+        memcpy(p_dat + 1, dst_addr, len);
+        *p_dat = 0;
+        send_frame(frame, len + 1);
 
-    } else if (subs == 0x20 && frame_len > 9) {
-        uint32_t addr = get_unaligned32(frame->dat + 5);
-        uint8_t len = frame->dat[2] - 6;
-        uint8_t ret = flash_write(addr, len, frame->dat + 9);
-        frame->dat[3] = ret ? 0x41 : 0x40;
-        frame->dat[2] = 1;
-        send_frame(frame);
+    } else if (*p_dat == 0x20 && p_len > 8) {
+        uint32_t addr = get_unaligned32(p_dat + 1);
+        uint8_t len = p_len - 5;
+        uint8_t ret = flash_write(addr, len, p_dat + 5);
+        *p_dat = ret ? 1 : 0;
+        send_frame(frame, 1);
 
     } else {
         cd_list_put(&frame_free_head, frame);
@@ -96,42 +92,39 @@ static void p8_hander(cd_frame_t *frame)
 }
 
 // csa manipulation
-static void p5_hander(cd_frame_t *frame)
+static void p5_handler(cd_frame_t *frame)
 {
     uint32_t flags;
-    uint8_t subs = frame->dat[4];
-    uint8_t frame_len = frame->dat[2];
+    uint8_t *p_dat = frame->dat + 5;
+    uint8_t p_len = frame->dat[2] - 2;
 
-    if (subs == 0x00 && frame_len == 5) {
-        uint16_t offset = get_unaligned16(frame->dat + 5);
-        uint8_t len = min(frame->dat[7], CDN_MAX_DAT - 1);
+    if (*p_dat == 0x00 && p_len == 4) {
+        uint16_t offset = get_unaligned16(p_dat + 1);
+        uint8_t len = min(p_dat[3], CDN_MAX_DAT - 1);
         local_irq_save(flags);
-        memcpy(frame->dat + 4, ((void *) &csa) + offset, len);
+        memcpy(p_dat + 1, ((void *) &csa) + offset, len);
         local_irq_restore(flags);
-        frame->dat[3] = 0x40;
-        frame->dat[2] = len + 1;
-        send_frame(frame);
+        *p_dat = 0;
+        send_frame(frame, len + 1);
 
-    } else if (subs == 0x20 && frame_len > 4) {
-        uint16_t offset = get_unaligned16(frame->dat + 5);
-        uint8_t len = frame_len - 4;
-        uint8_t *src_addr = frame->dat + 7;
+    } else if (*p_dat == 0x20 && p_len > 3) {
+        uint16_t offset = get_unaligned16(p_dat + 1);
+        uint8_t len = p_len - 3;
+        uint8_t *src_addr = p_dat + 3;
         uint16_t start = clip(offset, 0, sizeof(csa_t));
         uint16_t end = clip(offset + len, 0, sizeof(csa_t));
         local_irq_save(flags);
         memcpy(((void *) &csa) + start, src_addr + (start - offset), end - start);
         local_irq_restore(flags);
-        frame->dat[3] = 0x40;
-        frame->dat[2] = 1;
-        send_frame(frame);
+        *p_dat = 0;
+        send_frame(frame, 1);
 
-    } else if (subs == 0x01 && frame_len == 5) {
-        uint16_t offset = get_unaligned16(frame->dat + 5);
-        uint8_t len = min(frame->dat[7], CDN_MAX_DAT - 1);
-        memcpy(frame->dat + 4, ((void *) &csa_dft) + offset, len);
-        frame->dat[3] = 0x40;
-        frame->dat[2] = len + 1;
-        send_frame(frame);
+    } else if (*p_dat == 0x01 && p_len == 4) {
+        uint16_t offset = get_unaligned16(p_dat + 1);
+        uint8_t len = min(p_dat[3], CDN_MAX_DAT - 1);
+        memcpy(p_dat + 1, ((void *) &csa_dft) + offset, len);
+        *p_dat = 0;
+        send_frame(frame, len + 1);
 
     } else {
         cd_list_put(&frame_free_head, frame);
@@ -144,12 +137,12 @@ static inline void serial_cmd_dispatch(void)
     cd_frame_t *frame = cd_list_get(&d_dev.rx_head);
 
     if (frame) {
-        uint8_t server_num = frame->dat[3];
+        uint8_t server_num = frame->dat[4];
 
         switch (server_num) {
-        case 1: p1_hander(frame); break;
-        case 5: p5_hander(frame); break;
-        case 8: p8_hander(frame); break;
+        case 1: p1_handler(frame); break;
+        case 5: p5_handler(frame); break;
+        case 8: p8_handler(frame); break;
         default:
             printf("cmd err\n");
             cd_list_put(&frame_free_head, frame);
@@ -187,8 +180,8 @@ int _write(int file, char *data, int len)
             frm->dat[0] = 0xff;
             frm->dat[1] = 0x0;
             frm->dat[2] = 2 + len;
-            frm->dat[3] = 0x9;
-            frm->dat[4] = 0x40;
+            frm->dat[3] = 64;
+            frm->dat[4] = 9;
             memcpy(frm->dat + 5, data, len);
             cd_list_put(&d_dev.tx_head, frm);
             //return len;

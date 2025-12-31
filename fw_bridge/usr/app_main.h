e5f198a7d2d5f4d0f73e3618c2b85ee28e08e5e5
Duke <d@d-l.io>
2025-12-31 12:10:41 +0800

    add traditional half-duplex mode

--- fw_bridge/usr/app_main.h
+++ fw_bridge/usr/app_main.h
@@ -26,7 +26,7 @@
 
 #define BL_ARGS             0x20000000 // first word
 #define APP_CONF_ADDR       0x0801F800 // last page
-#define APP_CONF_VER        0x0202
+#define APP_CONF_VER        0x0203
 
 #define FRAME_MAX           80
 
